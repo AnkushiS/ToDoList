7ee043349db723880a98a15c7b91ca01446c81d1
AnkushiS <ankushi.sharma@infosys.com>
2015-09-10 10:40:48 -0400

    added the view lists tab and sql script

--- ToDoList/src/Lists.java
+++ ToDoList/src/Lists.java
@@ -14,12 +14,14 @@ import customTools.UserDB;
 
 @ManagedBean
 @SessionScoped
-public class List {
+public class Lists {
     private UserCh user;
     private ToDoList dolist;
     private String message;
     
-    public List() {
+    public List<ToDoList> view_list;
+
+    public Lists() {
     	
     }
     
@@ -54,22 +56,45 @@ public class List {
      	dolist.setStatusid(Double.valueOf(statusid));
      	
         UserDB.insertNewList(dolist);
-        viewlist(user.getUserid());
+        ListCatalog(user.getUserid());
+        view_list = getView_list();
         return "viewlist";
     }
     
-	public static List<ToDoList> viewlist(int userid) {
-		return UserDB.selectList(userid);
-			
+	
+	public void ListCatalog(int userid) {
+        view_list = new ArrayList<ToDoList>();
+        List<ToDoList> getLists =  UserDB.selectList(userid);
+        
+        for(ToDoList allLists : getLists){
+        	view_list.add(allLists);
+        	
+        }
+        
+    }
+
+	public List<ToDoList> getView_list() {
+		return view_list;
 	}
-	
-	
+
+	public void setView_list(List<ToDoList> view_list) {
+		this.view_list = view_list;
+	}
 
 	
 	
+	 public String ViewList() {
+		 FacesContext fc = FacesContext.getCurrentInstance();
+		 user = UserDB.selectUser(getParam(fc, "useremail"));
+		 
+		 ListCatalog(user.getUserid());
+	     view_list = getView_list();
+		 return "viewlist";    	
+	      
+	    } 
 	
 	
-
+	
 	private String getParam(FacesContext fc, String param_name) {
 			
 			Map<String,String> params = fc.getExternalContext().getRequestParameterMap();
